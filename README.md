d5cf57dcbdfb5652fbdc3e69b2ea3bfe0485336b
Matheus Couto <git.matheusvcouto@gmail.com>
2023-11-02 22:03:34 -0300

    funcionando como esperado

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-## Jogo da Advinhação / guessing-game
+# <p align="">Jogo da Advinhação / guessing-game</p>
 
 ![img da aplicação](src/img/img.png)
+
+### <p align="center">This projet was developed using vite and typescript</p>
+
+<p align="center">
+  <a href="https://vitejs.dev" target="_blank">
+    <img src="public/vite.svg" alt="Vite logo" width="70" style="margin-right: 20px;" /> <!-- Espaço adicionado aqui -->
+  </a>
+  <a href="https://www.typescriptlang.org/" target="_blank">
+    <img src="src/svg/typescript.svg" alt="TypeScript logo" width="70" />
+  </a>
+</p>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,43 @@
+
+const screen1 = document.querySelector('#screen1') as HTMLDivElement
+const screen2 = document.querySelector('#screen2') as HTMLDivElement
+const screenReseult = document.querySelector('#result') as HTMLParagraphElement
+
 function handleClick(event: Event) {
   event.preventDefault()
 
   const input = document.querySelector('#input') as HTMLInputElement
-  const { value } = input
-  const IntValue = parseInt(value)
 
-  if (!value || IntValue < 1 || IntValue > 10) {
+  // const { value } = input
+  const value = parseInt(input.value)
+
+  if (!value || value < 1 || value > 10) {
     return alert('Digite algum número de 1 a 10')
   }
 
-  console.log(value)
+  const randomNuber = Math.round(Math.random() * 10)
+  const result: boolean = value === randomNuber
+
+  screen1.classList.add('hidden')
+  screen2.classList.remove('hidden')
+
+  if (result == true) {
+    screenReseult.innerText = `Parabens você acertou!`
+  } else {
+    screenReseult.innerText = `Você errou, seu numero: ${value} o numero correto ${randomNuber}`
+  }
+  
+
+
+  console.log(value, randomNuber)
+}
+
+
+function playAgain(e: Event) {
+  e.preventDefault()
+
+  console.log('clickakcsas ')
+  screen1.classList.toggle('hidden')
+  screen2.classList.toggle('hidden')
+
 }
